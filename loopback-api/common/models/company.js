eb34240425401c2ab3fc46cea37a1e1b8c04940c
Yeong Sheng, Tan <yeongsheng.tan@ruckuswireless.com>
2017-09-29 12:31:11 +0800

    feat: init with work done till loopback custom-middleware

---
+++ loopback-api/common/models/company.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function(Company) {
+    Company.greet = function(msg, cb){
+        cb(null, msg);
+    }
+
+    Company.listCompanies = function(cb){
+        Company.find()
+            .then(companies => {
+                cb(null, companies);
+            })
+            .catch(err => {
+                cb(err);
+            })
+    }
+   /** 
+    * Say greetings
+    * @param {string} msg Message for greeting
+    * @param {Function(Error, string)} callback
+    */
+
+    Company.myGreet = function(msg, callback) {
+        callback(null, msg);
+    };``
+
+    Company.remoteMethod('greet', {
+        accepts : {
+            arg : 'msg',
+            type: 'string'
+        },
+        returns : {
+            arg : 'greeting',
+            type: 'string'
+        },
+        http : {
+            verb : 'get'
+        }
+    });
+};
---
+++ loopback-api/server/boot/automigrate.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function(app) {
+    app.dataSources.mongodb.automigrate(['Product', 'Company', 'Category', 'Account'], err => {
+        if(err) throw err;
+        console.log('Model Synced!');
+    })
+}
